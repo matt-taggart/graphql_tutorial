fd78da464b16cf2fbc8ab572c10a92099a88d0e0
Matthew Christopher Taggart <mtaggart89@gmail.com>
2017-08-13 12:03:36 -0400

    Added schema & resolvers for various pieces of functionality such as creating a user and user sign-in.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
 const schema = require('./schema');
+const connectMongo = require('./mongo-connector');
 const PORT = 3000;
 
-const app = express();
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-app.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql'
-}));
+const start = async () => {
+  const mongo = await connectMongo();
+  const app = express();
 
-app.listen(PORT, () => console.log(`Graphql server running on PORT ${PORT}`));
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
+    context: { mongo },
+    schema
+  }));
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
+  }));
+
+  app.listen(PORT, () => console.log(`Graphql server running on PORT ${PORT}`));
+}
+
+start();
--- src/schema/index.js
+++ src/schema/index.js
@@ -3,9 +3,29 @@ const resolvers = require('./resolvers');
 
 const typeDefs = `
   type Link {
-    id: ID!,
-    url: String!,
+    id: ID!
+    url: String!
     description: String!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String
+  }
+
+  input AUTH_PROVIDER_EMAIL {
+    email: String!
+    password: String!
+  }
+
+  input AuthProviderSignupData {
+    email: AUTH_PROVIDER_EMAIL
+  }
+
+  type SigninPayload {
+    token: String
+    user: User
   }
 
   type Query {
@@ -14,6 +34,8 @@ const typeDefs = `
 
   type Mutation {
     createLink(url: String!, description: String!): Link
+    createUser(name: String!, authProvider: AuthProviderSignupData!): User
+    signInUser(email: AUTH_PROVIDER_EMAIL): SigninPayload!
   }
 `;
 
